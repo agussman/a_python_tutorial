fb429ad30eab2f308cfb4b3a3ee4a9be599c4805
Aaron Gussman <agussman@gmail.com>
2020-08-19 22:44:28 -0400

    Some simple examples

---
+++ 202_Useful_Modules/useful_modules.py
@@ -0,0 +1,23 @@
+# random numbers
+import random
+
+# Print 5 random integers between 0 and 9, inclusive
+for i in range(5):
+    print(random.randrange(0, 10))
+
+# json
+import json
+
+txt = '{ "id": 300, "name": "Nancy", "scores": [3.4, 2.9, 4.4, 3.4]}'
+j = json.loads(txt)
+for key, value in j.items():
+    print(f"Key: {key} -> Value: {value}")
+
+# csv
+import csv
+
+input_csv = "../201_Command-line_Interfaces/data/va_cities.csv"
+with open(input_csv, 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+    for row in csvreader:
+        print(row[8])
